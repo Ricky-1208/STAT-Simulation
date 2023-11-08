f838cf4dcf8428a343421b1cd511bbd2b1b6eb04
Ricky.W <98917676+Ricky-1208@users.noreply.github.com>
2023-11-07 19:21:54 -0600

    Update 1.R

--- 1.R
+++ 1.R
@@ -3,3 +3,12 @@ rtri3 = function(n)
 ## generate a realization of Z_1,...,Z_n iid with ## density g by the inversion method z.list=3*sqrt(runif(n))
 return(z.list)
 }
+
+set.seed(123)
+n=2e3
+z.list=rtri3(n=n)
+## make the QQ plot
+probs=seq(from=0.01, to=0.99, by=0.01)
+plot(3*sqrt(probs) , quantile(z.list, probs),
+     xlab="Expected percentile", ylab="Data percentile")
+abline(0,1)
